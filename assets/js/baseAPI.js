1d85e0357b0a33f1484d3e36d238b16644fb9398
rowG <qf1ee@qq.com>
2021-05-22 19:36:07 +0800

    test

--- assets/js/baseAPI.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: rowG
- * @Date: 2021-05-21 16:48:37
- * @LastEditTime: 2021-05-21 17:16:39
- * @FilePath: \Liqifeng\assets\js\baseAPI.js
- */
-$.ajaxPrefilter(function (options) {
-	options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
-});
